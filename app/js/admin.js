4f5a7a1a1d7ef7e503c43b8af7916acb8435e55b
Yawenina <xiaoywkate@163.com>
2016-07-18 23:36:10 +0800

    complete user auth and upload data at admin.html

--- app/js/admin.js
+++ app/js/admin.js
@@ -1,51 +1,3 @@
-//admin component
-Vue.component('admin',{
-	template:'#admin',
-	data:function(){
-		return {
-			email:'',
-			password:'',
-			username:'',
-		}
-	},
-	computed:{
-		status:function(){
-			return this.username == '' ? false : true
-		}
-	},
-	methods:{
-		login:function(){
-			let provider = new firebase.auth.GoogleAuthProvider()
-			let that = this
-			firebase.auth().signInWithPopup(provider).then(function(result){
-				that.status = true
-				that.username = result.user.email
-				that.$emit('onAuthStateChanged')
-			}).catch(function(error){
-				alert(error.message)
-			})
-		},
-		logout:function(){
-			let that = this
-			firebase.auth().signOut().then(function(){
-				alert('你已成功退出！')
-				that.status = false
-			})
-		}
-	},
-	events:{
-		onAuthStateChanged:function(user){
-			if(user){
-				if(user.email != 'yawenina@google.com'){
-					firebase.auth().signOut()
-					alert('请登录正确用户！')
-					this.status = false
-				}
-			}
-		}
-	}
-})
-
 
 //add component
 Vue.component('add',{
@@ -53,68 +5,114 @@ Vue.component('add',{
 	data:function(){
 		return {
 			demo:{
-				name:'hi',
+				name:'',
 				date:'',
 				description:'',
 				github:'',
-				tag:'',
+				tag:'HTML',
 				imgUrl:''
 			},
 			val:0
 		}
 	},
 	methods:{
-		//上传数据
-		'addDemo':function(){
-			//检测用户是否为管理员登录
-			if(firebase.auth().currentUser.email == 'yawenina@gmail.com'){
-				//检测是否填充必要数据
-				if(this.demo.name && this.demo.description && this.demo.tag && this.demo.github){
-					//上传数据
-					firebase.database().ref().child('demos').push(this.demo).then(function(){
-						alert('上传成功！')
-					})
-					.catch(function(error){
-						console.log(error)
-					})
-				}
-			}else{
-				alert('请使用管理员登录！')
-			}
-			
-		},
 		//上传图片
 		sendImg:function(e){
 			let that = this
 			let file = e.target.files[0]
 			//检查上传文件格式
-			if(file.name.match(/[$jpg | png | gif]/)){
+			if(file.name.match(/[$.jpg | .png | .gif]/)){
 				//上传文件
 				let uploadTask = firebase.storage().ref().child('images/'+file.name).put(file)
 				//监测上传进度
 				uploadTask.on('state_changed',function(snapshot){
 					//设置进度条
-					that.val = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-					console.log('Upload is' + that.val + '% done')
+                    let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+					that.val = parseInt(progress)
 				},function(error){
 					console.log(error.message)
 				},function(){
 					//成功后设置图片的url地址
 					that.demo.imgUrl = uploadTask.snapshot.downloadURL
+                    file = null
 				})
 			}else{
 				alert('只支持上传png/jpg/gif格式的文件！')
 			}
-			
-		}
+		},
+        //上传数据
+        'addDemo':function(){
+            let that = this
+            //检测是否填充必要数据
+            if(this.demo.name && this.demo.description && this.demo.tag && this.demo.github && this.demo.imgUrl){
+                //上传数据
+                firebase.database().ref().child('demos').push(this.demo).then(function(){
+                    alert('上传成功！')
+                    //清空已填内容
+                    for(let key in that.demo){
+                        that.demo[key] = ''
+                    }
+                    that.val = 0
+                }).catch(function(error){
+                    console.log(error)
+                })
+            }else{
+                alert('请完善必填项目')
+            }
+        }
 	}
 })
 
 
-// var demos = firebase.database().ref().child('demos')
-new Vue({
-	el:'.container',
+var vm = new Vue({
+	el:'body',
 	data:{
 		currentView:'add',
+        email:''
 	},
+	methods:{
+		//用户登录
+		login:function(){
+			//绑定this
+			let that = this
+			//绑定Google登录
+			let provider = new firebase.auth.GoogleAuthProvider()
+			firebase.auth().signInWithPopup(provider).then(function(result){
+                //若为管理员登录则赋值email，否则退出
+				if(result.user.email == 'yawenina@gmail.com'){
+                    that.email = result.user.email
+                }else{
+                    that.logout()
+                }
+			}).catch(function(error){
+				console.log(error.message)
+			})
+		},
+		//用户退出
+		logout:function(){
+            let that = this
+			firebase.auth().signOut().then(function () {
+                that.email = ''
+				alert('您已成功退出！')
+			})
+		},
+        currentUser:function(){
+            let user = firebase.auth().currentUser
+            console.log(user)
+            //检查用户是否已登录,已登录则给email赋值，否则登录
+            if(user != null && user.email == 'yawenina@google.com'){
+                this.email = user.email
+            }
+            console.log('hi')
+        }
+	},
+    ready:function(){
+        //ready时检测是否用户已登录
+        let that = this
+        firebase.auth().onAuthStateChanged(function(user){
+            if(user){
+                that.email = user.email
+            }
+        })
+    }
 })
